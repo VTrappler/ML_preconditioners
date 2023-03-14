18d12c4e4898b791b983aba03b5d669c622d3118
VTrappler <victor.trappler.external@atos.net>
2023-03-14 10:39:04 +0000

    rename some files

--- smoke_hpo/hpo.py
+++ smoke_hpo/hpo.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("..")
 from prec_data.data import TangentLinearDataModule
 from prec_models.models_unstructured import BandMatrix, LowRank
+from prec_models.models_spectral import SVDConvolutional
 import torch
 import pytorch_lightning as pl
 import numpy as np
@@ -34,48 +35,47 @@ def construct_model_class(cl, **kwargs):
 
 @mlflow_mixin
 def trainable(config):
-    with mlflow.start_run(nested=True):
-        setup_mlflow(config=config, experiment_name=config["mlflow"]["experiment_name"])
-        metrics = {"loss": "Loss/val_loss"}
-        mlflow.pytorch.autolog()
-        state_dimension = config["data"]["dimension"]
-        data_path = config["data"]["data_path"]
+    setup_mlflow(config=config, experiment_name=config["mlflow"]["experiment_name"])
+    metrics = {"loss": "Loss/val_loss"}
+    mlflow.pytorch.autolog()
+    state_dimension = config["data"]["dimension"]
+    data_path = config["data"]["data_path"]
 
-        torch_model = construct_model_class(LowRank, rank=config["architecture"]["rank"])
-        mlflow.log_params(config["architecture"])
-        mlflow.log_params(config["data"])
-        # mlflow.log_params(config["optimizer"])
+    torch_model = construct_model_class(LowRank, rank=config["architecture"]["rank"])
+    mlflow.log_params(config["architecture"])
+    mlflow.log_params(config["data"])
+    # mlflow.log_params(config["optimizer"])
 
-        model = torch_model(state_dimension=state_dimension, config=config["architecture"])
-        datamodule = TangentLinearDataModule(
-            path=data_path,
-            batch_size=config["architecture"]["batch_size"],
-            num_workers=4,
-            splitting_lengths=[0.8, 0.1, 0.1],
-            shuffling=True,
-        )
-        trainer = pl.Trainer(
-            max_epochs=config["optimizer"]["epochs"],
-            callbacks=[TuneReportCallback(metrics, on="validation_end")],
-            enable_progress_bar=False,
-        )
+    model = torch_model(state_dimension=state_dimension, config=config["architecture"])
+    datamodule = TangentLinearDataModule(
+        path=data_path,
+        batch_size=config["architecture"]["batch_size"],
+        num_workers=4,
+        splitting_lengths=[0.8, 0.1, 0.1],
+        shuffling=True,
+    )
+    trainer = pl.Trainer(
+        max_epochs=config["optimizer"]["epochs"],
+        callbacks=[TuneReportCallback(metrics, on="validation_end")],
+        enable_progress_bar=False,
+    )
 
-        test_input = torch.normal(
-            0, 1, size=(config["architecture"]["batch_size"], state_dimension)
-        )
-        forw = model.forward(test_input)
-        print(f"{forw.shape=}")
+    test_input = torch.normal(
+        0, 1, size=(config["architecture"]["batch_size"], state_dimension)
+    )
+    forw = model.forward(test_input)
+    print(f"{forw.shape=}")
 
-        #     mats = model.construct_full_matrix(forw)
-        #     print(f"{mats.shape=}")
-        #     print(f"{mats}")
-        signature = mlflow.models.signature.infer_signature(
-            test_input.detach().numpy(), forw.detach().numpy()
-        )
-        trainer.fit(
-            model,
-            datamodule,
-        )
+    #     mats = model.construct_full_matrix(forw)
+    #     print(f"{mats.shape=}")
+    #     print(f"{mats}")
+    signature = mlflow.models.signature.infer_signature(
+        test_input.detach().numpy(), forw.detach().numpy()
+    )
+    trainer.fit(
+        model,
+        datamodule,
+    )
         # mlflow.pytorch.log_model(model, "smoke_model", signature=signature)
 
 
